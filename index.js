d124850d46284220887a4cd2780e64228334c43a
mehedi hasan <tanjildewan10@gmail.com>
2023-08-06 20:51:35 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -25,11 +25,51 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const categorys = client.db("news-hub").collection("categorys");
+    const categorysCollection = client.db("news-hub").collection("categorys");
+    const newsCollection = client.db("news-hub").collection("all-news")
+    const saveNewsCollection = client.db("news-hub").collection("save-news")
 
-    app.get("/category",async(req,res)=>{
-        const result = await categorys.find().toArray()
+    app.get("/category", async (req, res) => {
+      const result = await categorysCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get("/category/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { id: id }
+      const result = await categorysCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.get("/news", async (req, res) => {
+      const result = await newsCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get("/news/:id", async (req, res) => {
+      const id = req.params.id;
+      if (id == 0) {
+        const result = await newsCollection.find().toArray()
         res.send(result)
+      }
+      else {
+        const query = { category_id: id }
+        const result = await newsCollection.find(query).toArray()
+        res.send(result)
+      }
+    })
+
+    app.get("/saveNews",async(req,res)=>{
+      const email = req.query.email;
+      const query = {email:email}
+      const result = await saveNewsCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.post("/saveNews", async (req, res) => {
+      const data = req.body
+      const result = await saveNewsCollection.insertOne(data)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
